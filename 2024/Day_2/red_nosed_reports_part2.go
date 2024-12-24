7f3b9b428e4827831dcb33b8f8daa65cd0901d1c
Chandler V. Staggs <chandler.staggs945@topper.wku.edu>
2024-12-23 20:58:14 -0600

    AoC 2024 third day

--- 2024/Day_2/red_nosed_reports_part2.go
+++ 2024/Day_2/red_nosed_reports_part2.go
@@ -3,40 +3,39 @@ package main
 import (
 	"AoC/utils"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
-var count int = 0
+func absInt(x int) int {
+	return absDiffInt(x, 0)
+}
 
-// func absInt(x int) int {
-// 	return absDiffInt(x, 0)
-// }
+func absDiffInt(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
 
-// func absDiffInt(x, y int) int {
-// 	if x < y {
-// 		return y - x
-// 	}
-// 	return x - y
-// }
+func absDiffUint(x, y uint) uint {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
 
-// func absDiffUint(x, y uint) uint {
-// 	if x < y {
-// 		return y - x
-// 	}
-// 	return x - y
-// }
-
-func recurse_safe_report(report []int) bool {
-	count += 1
+func recurse_safe_report(report []int, count int) bool {
 	if count > 1 {
-		count = 0
 		return false
 	}
-	ret := safe_report_part2(report)
-	count = 0
+	ret := safe_report_part2(report, count+1)
 	return ret
 }
 
-func safe_report_part2(report []int) bool {
+func safe_report_part2(report []int, count int) bool {
 	/*
 		The levels are either all increasing or all decreasing.
 		Any two adjacent levels differ by at least one and at most three.
@@ -53,63 +52,66 @@ func safe_report_part2(report []int) bool {
 			} else if report[i] > report[i-1] {
 				descending = false
 			} else {
-				return recurse_safe_report(utils.RemoveIndex(report, i))
+				return recurse_safe_report(utils.RemoveIndex(report, i), count+1) || recurse_safe_report(utils.RemoveIndex(report, i-1), count+1)
 			}
 			orderset = true
 		} else {
 			if descending {
 				if report[i] > report[i-1] {
-					return recurse_safe_report(utils.RemoveIndex(report, i)) || recurse_safe_report(utils.RemoveIndex(report, i-1))
+					return recurse_safe_report(utils.RemoveIndex(report, i), count+1) || recurse_safe_report(utils.RemoveIndex(report, i-1), count+1)
 				}
 			} else {
 				if report[i] < report[i-1] {
-					return recurse_safe_report(utils.RemoveIndex(report, i)) || recurse_safe_report(utils.RemoveIndex(report, i-1))
+					return recurse_safe_report(utils.RemoveIndex(report, i), count+1) || recurse_safe_report(utils.RemoveIndex(report, i-1), count+1)
 				}
 			}
 		}
 
 		diff := absInt(report[i] - report[i-1])
 		if diff < 1 || diff > 3 {
-			return recurse_safe_report(utils.RemoveIndex(report, i))
+			return recurse_safe_report(utils.RemoveIndex(report, i), count+1) || recurse_safe_report(utils.RemoveIndex(report, i-1), count+1)
 		}
 
 	}
 	return true
 }
 
-// func parseNums(lines []string) [][]int {
-// 	reports := make([][]int, 0)
-// 	for _, line := range lines {
-// 		report := make([]int, 0)
-// 		strNumbers := strings.Fields(line)
-// 		for _, num := range strNumbers {
-// 			val, err := strconv.Atoi(num)
-// 			if err != nil {
-// 				log.Fatal(err, " Cannot convert value: ", val)
-// 			}
-// 			report = append(report, val)
+func parseNums(lines []string) [][]int {
+	reports := make([][]int, 0)
+	for _, line := range lines {
+		report := make([]int, 0)
+		strNumbers := strings.Fields(line)
+		for _, num := range strNumbers {
+			val, err := strconv.Atoi(num)
+			if err != nil {
+				log.Fatal(err, " Cannot convert value: ", val)
+			}
+			report = append(report, val)
 
-// 		}
-// 		reports = append(reports, report)
-// 	}
-// 	return reports
-// }
+		}
+		reports = append(reports, report)
+	}
+	return reports
+}
 
 func main() {
-	// var count int = 0
-	// filename := os.Args[1]
-	// lines := utils.ReadFile(filename)
-	// reports := parseNums(lines)
-	// for _, report := range reports {
-	// 	issafe := safe_report_part2(report)
-	// 	fmt.Println(report)
-	// 	fmt.Println(issafe)
-	// 	if issafe {
-	// 		count += 1
-	// 	}
-	// }
-	// fmt.Println("Count: ", count)
-	// fmt.Println(safe_report_part2([]int{25, 22, 19, 21, 20, 17, 14, 13}))
+	var safe_count int = 0
+	var recurse_count int = 0
+	filename := os.Args[1]
+	lines := utils.ReadFile(filename)
+	reports := parseNums(lines)
+	for _, report := range reports {
+		issafe := safe_report_part2(report, recurse_count)
+		fmt.Println(report)
+		fmt.Println(issafe)
+		if issafe {
+			safe_count += 1
+		}
+	}
+	fmt.Println("Count: ", safe_count)
+	// fmt.Println(safe_report_part2([]int{25, 22, 19, 21, 20, 17, 14, 13}, count))
 
-	fmt.Println(safe_report_part2([]int{30, 27, 29, 26, 28}))
+	// fmt.Println(safe_report_part2([]int{30, 27, 29, 26, 28}, count))
+	// fmt.Println(safe_report_part2([]int{86, 87, 88, 91, 91}, count))
+
 }
