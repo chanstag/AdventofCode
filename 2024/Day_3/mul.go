3ffc378244ee271373ac105b51c3ebfccf0d5e22
Chandler V. Staggs <chandler.staggs945@topper.wku.edu>
2024-12-24 22:20:17 -0600

    AoC 2024 refactor parse nums

--- 2024/Day_3/mul.go
+++ 2024/Day_3/mul.go
@@ -28,7 +28,8 @@ func parse_exp() []string {
 	return matches
 }
 
-func parse_nums(matches []string){
+func parse_nums(matches []string) [][]int {
+	var nums [][]int
 	exp := `([0-9]+),([0-9]+)`
 	r, err := regexp.Compile(exp)
 
@@ -36,14 +37,32 @@ func parse_nums(matches []string){
 		log.Fatal("Failed to compile regex")
 	}
 
-	for _, mul := range matches{
-		match := r.FindAllStringSubmatch(mul, -1)
+	for _, mul := range matches {
+		match := r.FindStringSubmatch(mul)
 		num1, err := strconv.Atoi(match[1])
-
+		if err != nil {
+			log.Fatal("issue converting string")
+		}
+		num2, err := strconv.Atoi(match[2])
+		if err != nil {
+			log.Fatal("issue converting string")
+		}
+		nums = append(nums, []int{num1, num2})
 	}
+	return nums
+}
+
+func multiply(nums [][]int) int {
+	var total int = 0
+	for _, num := range nums {
+		total += num[0] * num[1]
+	}
+	return total
 }
 
 func main() {
 	matches := parse_exp()
+	nums := parse_nums(matches)
 
+	fmt.Println(nums)
 }
