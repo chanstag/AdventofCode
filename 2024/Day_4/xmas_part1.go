662c6f4e29abc7690cb5ca50fa345ad9553ed499
Chandler Staggs <chanstag@live.com>
2025-01-26 19:07:49 -0600

    implementing sub-problem matches

--- 2024/Day_4/xmas_part1.go
+++ 2024/Day_4/xmas_part1.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"AoC/utils"
-	"fmt"
 )
 
 func match_xmas(word string) bool {
 	return word == "xmas"
 }
 
+/*
+
+ */
 /*
 match 4 rows
 match 4 columns
@@ -19,6 +21,7 @@ top-right-to-bottom-left
 */
 func process_window() {
 	lines := utils.ReadFile("xmas.txt")
+	var total_matches = 0
 	//match 4 rows
 	current_row := 0
 	current_col := 0
@@ -31,23 +34,91 @@ func process_window() {
 			if len(line)-j < 4 {
 				break
 			}
-			match_rows(current_row, current_col, lines)
+			total_matches += match_row(current_row, current_col, lines) + match_col(current_row, current_col, lines) + match_top_left_right_diagnol(current_row, current_col, lines)
+
 			current_col = j
 		}
 		current_row = i
 	}
 }
-
-func match_rows(current_row int, current_col int, lines []string) {
-	// total_matches := 0
-	current_window := lines[current_row : current_row+4]
-	for i, row := range current_window {
-		chars := row[current_col : current_col+4]
-		fmt.Printf("index %d: %s ", i, chars)
+func match_top_left_right_diagnol(current_row int, current_col int, lines []string) int {
+	var chars []rune
+	if len(lines)-1-current_row < 4 || len(lines[0])-1-current_col < 4 {
+		return 0
 	}
+
+	for i := range 4 {
+		chars = append(chars, rune(lines[current_row+i][current_col+i]))
+	}
+	if match_xmas(string(chars)) {
+		return 1
+	}
+
+	return 0
+
+}
+
+func match_bot_right_left_diagnol(current_row int, current_col int, lines []string) int {
+	var chars []rune
+
+	for i := range 4 {
+		chars = append(chars, rune(lines[current_row+4-i][current_col+4-i]))
+	}
+
+	if match_xmas(string(chars)) {
+		return 1
+	}
+	return 0
+}
+
+func match_bot_left_right_diagnol(current_row int, current_col int, lines []string) int {
+	var chars []rune
+	if current_row < 4 || len(lines[0])-1-current_col < 4 {
+		return 0
+	}
+
+	for i := range 4 {
+		chars = append(chars, rune(lines[current_row+4-i][current_col+4-i]))
+	}
+	if match_xmas(string(chars)) {
+		return 1
+	}
+	return 0
+}
+
+func match_row(current_row int, current_col int, lines []string) int {
+	chars := lines[current_row][current_col : current_col+4]
+
+	if match_xmas(string(chars)) {
+		return 1
+	}
+	return 0
+}
+
+func match_col(current_row int, current_col int, lines []string) int {
+	chars := lines[current_row : current_row+4][current_col]
+
+	if match_xmas(string(chars)) {
+		return 1
+	}
+	return 0
 
 }
 
 func main() {
 	process_window()
+	// lines := []string{
+	// 	"xmas",
+	// 	"masx",
+	// 	"asxm",
+	// 	"sxma",
+	// }
+	// // chars := lines[0 : 0+4][1]
+	// // fmt.Printf("%s", chars)
+	// var chars []rune
+	// for i := range 4 {
+	// 	chars = append(chars, rune(lines[0+i][0+i]))
+	// }
+	// fmt.Printf("%s", string(chars))
+	return
 }
