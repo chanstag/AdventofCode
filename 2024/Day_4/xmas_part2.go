1b7b541ae5457d469b3bcf3e13c737628bd94144
Chandler Staggs <chanstag@live.com>
2025-02-11 23:59:06 -0600

    stuff

--- 2024/Day_4/xmas_part2.go
+++ 2024/Day_4/xmas_part2.go
@@ -8,7 +8,7 @@ import (
 
 func match_xmas(word string) bool {
 	word = strings.ToUpper(word)
-	return word == "XMAS"
+	return word == "MAS"
 }
 
 /*
@@ -19,10 +19,10 @@ top-right-to-bottom-left
 */
 func process_window(lines []string) int {
 	var total_matches = 0
-	var total_top_left_right = 0
-	var total_bot_right_left = 0
-	var total_top_right_left = 0
-	var total_bot_left_right = 0
+	var top_left_right = 0
+	var bot_right_left = 0
+	var top_right_left = 0
+	var bot_left_right = 0
 
 	//match 4 rows
 	current_row := 0
@@ -52,19 +52,19 @@ func process_window(lines []string) int {
 			}
 
 		}
-
 	}
-	fmt.Printf("%d %d %d %d %d %d %d %d", total_top_left_right, total_bot_right_left, total_top_right_left, total_bot_left_right)
+
+	fmt.Printf("%d %d %d %d %d %d %d %d", top_left_right, bot_right_left, top_right_left, bot_left_right)
 	return total_matches
 }
 
 func match_top_right_left_diagnol(current_row int, current_col int, lines []string) bool {
 	var word = ""
-	if len(lines)-current_row < 4 || current_col < 3 {
+	if len(lines)-current_row < 3 || current_col < 2 {
 		return false
 	}
 
-	for i := range 4 {
+	for i := range 3 {
 		word += string(lines[current_row+i][current_col-i])
 	}
 	if match_xmas(string(word)) {
@@ -76,11 +76,11 @@ func match_top_right_left_diagnol(current_row int, current_col int, lines []stri
 
 func match_top_left_right_diagnol(current_row int, current_col int, lines []string) bool {
 	var word = ""
-	if len(lines)-current_row < 4 || len(lines[0])-current_col < 4 {
+	if len(lines)-current_row < 3 || len(lines[0])-current_col < 3 {
 		return false
 	}
 
-	for i := range 4 {
+	for i := range 3 {
 		word += string(lines[current_row+i][current_col+i])
 	}
 	if match_xmas(string(word)) {
@@ -93,7 +93,7 @@ func match_top_left_right_diagnol(current_row int, current_col int, lines []stri
 
 func match_bot_right_left_diagnol(current_row int, current_col int, lines []string) bool {
 	var word = ""
-	if current_row < 3 || current_col < 3 {
+	if current_row < 2 || current_col < 2 {
 		return false
 	}
 
@@ -109,7 +109,7 @@ func match_bot_right_left_diagnol(current_row int, current_col int, lines []stri
 
 func match_bot_left_right_diagnol(current_row int, current_col int, lines []string) bool {
 	var word = ""
-	if current_row < 3 || len(lines[0])-current_col < 4 {
+	if current_row < 2 || len(lines[0])-current_col < 3 {
 		return false
 	}
 
@@ -123,6 +123,6 @@ func match_bot_left_right_diagnol(current_row int, current_col int, lines []stri
 }
 
 func main() {
-	lines := utils.ReadFile("")
+	lines := utils.ReadFile("x_test")
 	process_window(lines)
 }
